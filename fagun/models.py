50ec5a0d9a099b02d762b879f6b360fd8cb06c58
Ernir <e.ernir@gmail.com>
2016-01-13 10:03:18 +0000

    Refactored models to inherit from a common "publishable entity"
    Most modifiable things on the page share common elements - they
    have titles, text bodies and visibility status.

--- fagun/models.py
+++ fagun/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 
 
-class SidebarEntry(models.Model):
+class PublishableEntity(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(blank=True)
-    priority = models.PositiveIntegerField(default=0)
+    visible = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        abstract = True
+
+
+class SidebarEntry(PublishableEntity):
+    priority = models.PositiveIntegerField(default=0)
 
     class Meta(object):
         verbose_name_plural = "sidebar entries"
         ordering = ("priority", )
 
 
-class NewsArticle(models.Model):
-    title = models.CharField(max_length=200)
+class NewsArticle(PublishableEntity):
     published_at = models.DateField()
     slug = models.SlugField()
-    body = models.TextField()
-
-    def __str__(self):
-        return self.title
 
     class Meta:
         ordering = ("published_at", )
